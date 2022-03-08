df47f4cc7c9144c6c5156aa92da60b794aef5498
Daiane da Rosa <daianedarosadev@gmail.com>
2022-03-07 22:43:18 -0300

    ✨ Nova pasta com  +4 arquivos

---
+++ aula1/index.js
@@ -0,0 +1,14 @@
+var nome = "Daiane"
+var notaPrimeiroBimestre = 8.2
+var notaSegundoBimestre = 9.23
+var notaTerceiroBimestre = 6.60
+var notaQuartoBimestre = 7.5
+
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
